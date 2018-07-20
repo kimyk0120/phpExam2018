59e206111b571fc063ad3ee82cba50525b26b6dc
YoungKim <kimyk0120@hotmail.com>
2018-07-20 17:45:48 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
+<!-- 
+	location : /Applications/MAMP/htdocs/phpExam2018
+	
+	실행방법 :
+		-  mamp/hodocs 에 프로젝트 확인 
+		-  mamp로 서버 구동
+		-  해당 php 파일 실행 
+ -->
+
 <?php
 echo "test"
 ?>
 <html>
-	<h2>test index </h2>
+	<h2>Hello World</h2>
 </html>
+
+
